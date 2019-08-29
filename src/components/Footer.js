d88d039b6dd243b8261ac51f5425b41789ccc501
Phil Prater <pprater92@gmail.com>
2019-08-29 16:22:03 -0700

    Added a small list of functional test links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Footer.css';
 //import ReactDOM from 'react-dom';
-
-// TO-DO const listOfLinks = [];
+const listOfLinks = (
+  <ul>
+    <a href="https://en.wikipedia.org/wiki/Parrot">Services(parrot)</a>
+    <a href="https://en.wikipedia.org/wiki/Mantis_shrimp">Contact Us(mantis shrimp)</a>
+    <a href="https://en.wikipedia.org/wiki/Gecko">Legal(gecko)</a>
+  </ul>
+);
 
 class Footer extends React.Component {
   render() {
     return(
       <footer>
-        Sneaky lil' footer
+        Sneaky lil' Footer
+        {listOfLinks}
       </footer>
     );
   }

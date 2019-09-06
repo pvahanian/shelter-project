aa68aebc45b84a4d24aa6645c015685b60fdecd3
trevorleake <trevorjohnleake@gmail.com>
2019-09-06 15:39:15 -0700

    Add male and female dropdown

--- src/components/SearchSidebar.js
+++ src/components/SearchSidebar.js
@@ -4,27 +4,42 @@ import './SearchSidebar.css';
 class SearchSidebar extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
+      sex: 'Male',
       zipCode: ''
     }
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleSexChange = this.handleSexChange.bind(this);
+    this.handleZipChange = this.handleZipChange.bind(this);
   }
 
-
-  handleChange(event) {
+  handleZipChange(event) {
     this.setState({zipCode: event.target.value});
   }
-
+  handleSexChange(event) {
+    this.setState({sex: event.target.value});
+  }
 
   render() {
     return (
       <div id="search-sidebar">
-        {/* Form Fields */}
-        Zip code:
-        <input type="text" value={this.state.zipCode} onChange={this.handleChange} />
+        <div id="search-sidebar-title">
+          Search Bar
+        </div>
 
+        {/* Form Fields */}
+        <div class="search-sidebar-field">
+          Zip
+          <input type="text" value={this.state.zipCode} onChange={this.handleZipChange} />
+        </div>
+
+        <div class="search-sidebar-field">
+          Sex
+          <select selected={this.state.sex} onChange={this.handleSexChange}>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </div>
 
         {/* Display */}
         <h1>

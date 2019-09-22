aa72874cc82381aa194beec63282d734745f3d69
Nathaniel <nathaniels2016@gmail.com>
2019-09-22 13:16:57 -0700

    changes to App.js and Index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Footer from './components/Footer.js';
+//import Footer from './components/Footer.js';
+//import FieldSelector from './components/FieldSelector';
 //import NavBlock from './components/NavigationBlock';
 
-ReactDOM.render(</*App*/Footer/*NavBlock*/ />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

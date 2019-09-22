6ed4ebbd99ab6d75e8d788bb264241561acfbf07
trevorleake <trevorjohnleake@gmail.com>
2019-09-22 14:20:44 -0700

    Add navbar to app.js. Swap old NavBar to NavBlock

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FieldSelector from "./components/FieldSelector";
-import NavBar from "./components/NavigationBlock";
+import NavBlock from "./components/NavigationBlock";
+import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+
+const navbar = {};
+navbar.brand =
+  {linkTo: "#", text: "Portland Shelters"};
+navbar.links = [
+  {linkTo: "#", text: "Contact Us"},
+  {linkTo: "#", text: "How many links do we need?"},
+  {dropdown: false, text: "Do we want a Dropdown?", links: [
+    {linkTo: "#", text: "Dropdown Link 1"},
+    {linkTo: "#", text: "Dropdown Link 2"}
+  ]}
+];
+
+
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar {...navbar} />
+      <NavBlock />
       <FieldSelector />
       <Footer />
     </div>

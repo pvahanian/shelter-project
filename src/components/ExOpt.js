11b4727774d275511af61953ba4fe2b22d050ec2
Phil Prater <pprater92@gmail.com>
2019-09-28 16:47:01 -0700

    Got buttons working with help from Trevor

--- src/components/ExOpt.js
+++ src/components/ExOpt.js
@@ -7,9 +7,52 @@ pressed).
 import React from 'react';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+ 
   render() {
     return (
-
+      <button 
+        className={this.props.selected ? 'pick':'not'}
+        onClick={
+          e => {this.props.onClick(e, this.props.name)}
+        }
+      >
+        {this.props.name}
+      </button>
     );
   }
-};
+}
+
+Square.defaultProps = {selected: false};
+
+class Group extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {pick: ''};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(event, name) {
+    this.setState({pick: name})
+  }
+
+  render() {
+    return (
+      <div> 
+        {
+        this.props.items.map((item, i) => 
+          <Square 
+            selected={item===this.state.pick}
+            key={i} 
+            name={item}
+            onClick={this.handleClick}
+            />)
+        }
+      </div>
+    );
+  }
+}
+
+const names = ['Bob', 'Joe', 'Dave'];

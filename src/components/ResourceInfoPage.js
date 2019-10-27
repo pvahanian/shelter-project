ebcd5fecaeadc79ac3ffab0a82253f76e150c2dd
Brian Allen <bdallen00@gmail.com>
2019-10-27 15:29:40 -0700

    added ResourceInfoPage work in progress

---
+++ src/components/ResourceInfoPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Shelter from './shelter_info.js';
+
+class ResourceInfoPage extends React.Component {
+  render() {
+    return Shelter("howdy","howdy","howdy","howdy","howdy");
+  }
+}
+export default ResourceInfoPage

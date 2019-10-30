49a100b41d964700fb77354e8b3bf77215a2f7c0
trevorleake <trevorjohnleake@gmail.com>
2019-10-29 21:17:02 -0700

    Create unsafe functions to hit all 211 endpoints

--- src/APIWrapper.js
+++ src/APIWrapper.js
@@ -5,25 +5,84 @@ class APIWrapper {
     this.parameters = {
       APIKey: APIKey
     }
+
+    this.serviceType = {
+      category: 'C',
+      subCategory: 'SC',
+      serviceName: 'S',
+      LocationOrProviderName: 'N'
+    }
+  }
+
+  async initialize() {
+    let data = await this.getSessionID()
+    this.parameters.sessionID = data[0]['session_id']
   }
 
   async getSessionID() {
-    await fetch(
+    let response = await fetch(
       `https://www.navigateopen.info/pubres/api/GetSessionID/?ip={apikey: "${this.parameters.APIKey}"}`
     )
-    .then((data) => {
-      return data.json()
-    })
+    let data = await response.json()
+    return data
   }
 
-  async getCategories(obj) {
-    await fetch(
-      `https://www.navigateopen.info/pubres/api/GetCategories/?ip={sid: "${obj.sessionID}", apikey: "${obj.APIKey}"}`
+  async getCategories() {
+    let callParams = JSON.parse(JSON.stringify(this.parameters))
+
+    let response = await fetch(
+      `https://www.navigateopen.info/pubres/api/GetCategories/?ip=${JSON.stringify(callParams)}`
     )
-    .then((data) => {
-      return data.json()
-    })
+    let data = await response.json()
+    return data
   }
+
+  async getCountyByZipCode(obj) {
+    let callParams = JSON.parse(JSON.stringify(this.parameters))
+    callParams = {...callParams, ...obj}
+
+    let response = await fetch(
+      `https://www.navigateopen.info/pubres/api/GetCounty/?ip=${JSON.stringify(callParams)}`
+    )
+    let data = await response.json()
+    return data
+  }
+
+  async getKeywords(obj) {
+    let callParams = JSON.parse(JSON.stringify(this.parameters))
+    callParams = {...callParams, ...obj}
+
+    let response = await fetch(
+      `https://www.navigateopen.info/pubres/api/GetCategories/?ip=${JSON.stringify(callParams)}`
+    )
+    let data = await response.json()
+    return data
+  }
+
+  async serviceNameSearch(obj) {
+    let callParams = JSON.parse(JSON.stringify(this.parameters))
+    callParams = {...callParams, ...obj}
+
+    console.log(callParams)
+
+    let response = await fetch(
+      `https://www.navigateopen.info/pubres/api/ServiceProviders/?ip=${JSON.stringify(callParams)}`
+    )
+
+    let data = await response.json()
+    return data
+  }
+
+  async detailDrilldown(obj) {
+    let callParams = JSON.parse(JSON.stringify(this.parameters))
+    callParams = {...callParams, ...obj}
+
+    let response = await fetch(
+      `https://www.navigateopen.info/pubres/api/ProviderDetail/?ip=${JSON.stringify(callParams)}`
+    )
+    let data = await response.json()
+    return data
+}
 
 
 

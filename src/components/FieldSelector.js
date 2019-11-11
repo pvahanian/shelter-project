08ce24e9530a332abe30cb2f8e5d1b054444b6ed
trevorleake <trevorjohnleake@gmail.com>
2019-11-11 12:42:24 -0800

    Use breakpoints for grid and font sizing & add color transition for buttons

--- src/components/FieldSelector.js
+++ src/components/FieldSelector.js
@@ -3,23 +3,26 @@ import ExclusiveOption from "./ExclusiveOption";
 import '../Assets/FieldSelector.scss';
 
 class FieldSelector extends React.Component {
-    render() {
-        return(
-        <div className={"field-selector"}>
-            <ExclusiveOption items={['Male', 'Female', 'Transgender Male', 'Transgender Female']}/>
-            <div className={"AgeSelector"}>
-                <input type={"number"} value={"2"}/>
-            </div>
-            <div className={"Location"}>
-                <p>Zip Code: <input type={"Number"}/></p>
-                <a href={"google.com"}>Your location</a>
-            </div>
-            <div className={"Services"}>
-            </div>
+  render() {
+    return(
+      <div className='field-selector'>
+        text
+        <ExclusiveOption items={['Male', 'Female', 'Transgender Male', 'Transgender Female']}/>
+
+        <div className={"AgeSelector"}>
+          <input type={"number"} value={"2"}/>
         </div>
 
-        );
-    }
+        <div className={"Location"}>
+          <p>Zip Code: <input type={"Number"}/></p>
+          <a href={"google.com"}>Your location</a>
+        </div>
+
+        <div className={"Services"}>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FieldSelector;

0da5b9b0f86146385cb7531aa40c5c5c93b46f71
trevorleake <trevorjohnleake@gmail.com>
2019-11-11 13:15:37 -0800

    Add NumberInput component and use in FieldSelector & add dark-border color variable

--- src/components/FieldSelector.js
+++ src/components/FieldSelector.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import ExclusiveOption from "./ExclusiveOption";
+import NumberInput from './NumberInput';
 import '../Assets/FieldSelector.scss';
 
 class FieldSelector extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      age: 25,
+      zip: 97205
+    }
+
+    this.handleAgeChange = this.handleAgeChange.bind(this)
+    this.handleZIPChange = this.handleZIPChange.bind(this)
+  }
+
+  handleAgeChange(e) {
+    let age = e.currentTarget.value
+    this.setState({ age: age })
+  }
+
+  handleZIPChange(e) {
+    let zip = e.currentTarget.value
+    this.setState({ zip: zip })
+  }
+
+
   render() {
     return(
       <div className='field-selector'>
         text
         <ExclusiveOption items={['Male', 'Female', 'Transgender Male', 'Transgender Female']}/>
+        <NumberInput name='Age' onChange={this.handleAgeChange} />
+        <NumberInput name='ZIP' onChange={this.handleZIPChange} />
 
-        <div className={"AgeSelector"}>
-          <input type={"number"} value={"2"}/>
-        </div>
-
-        <div className={"Location"}>
-          <p>Zip Code: <input type={"Number"}/></p>
-          <a href={"google.com"}>Your location</a>
-        </div>
+        <a href={"google.com"}>Your location</a>
 
         <div className={"Services"}>
         </div>

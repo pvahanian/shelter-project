51104a2dc8d6b2b6a1deffd3b9488741c5fb1c66
trevorleake <trevorjohnleake@gmail.com>
2019-11-13 12:22:51 -0800

    Add theme swap button

--- src/App.js
+++ src/App.js
@@ -30,18 +30,24 @@ const APIKey = process.env.REACT_APP_211_API_KEY
 const API = new APIWrapper(APIKey)
 
 class App extends React.Component {
-  themeColor = 'light'
 
   state = {
+    themeColor: 'light',
     sessionID: null,
     categories: []
   }
 
   render() {
     return (
-      <ThemeContext.Provider value={this.themeColor}>
-        <div className={'app ' + this.themeColor }>
+      <ThemeContext.Provider value={this.state.themeColor}>
+        <div className={'app ' + this.state.themeColor }>
           <div id='left-gutter-container'>
+            <button onClick={e => this.setState({
+              themeColor: this.state.themeColor === 'light' ? 'dark' : 'light'
+            })}>
+              Swap Theme
+            </button>
+
             Left Gutter
           </div>
 

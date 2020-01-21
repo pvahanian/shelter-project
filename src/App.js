020a1bc22e60ad34c4790c164dade94627cbdd3a
Brian Allen <bdallen00@gmail.com>
2020-01-20 17:09:43 -0800

    Catching branch up to master

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ import APIWrapper from "./APIWrapper.js";
 import ExclusiveOption from "./components/ExclusiveOption";
 import Section from './components/Section';
 import { ThemeContext } from './ThemeContext';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import Shelter from './components/shelter_info'
+
 
 const navbar = {};
 navbar.brand = {linkTo: "#", text: "Portland Shelters"};
@@ -53,7 +62,17 @@ class App extends React.Component {
 
           <div id='main-container'>
             Main Container
-            <FieldSelector />
+            <Route
+              exact path="/"
+            >
+              <FieldSelector changeAPIData={this.handleApiDataChange}/>
+            </Route>
+
+            <Route
+              path="/info"
+            >
+              <Shelter appProps={this.state}/>
+            </Route>
           </div>
 
           <div id='right-gutter-container'>
---
+++ src/components/SubmitButton/SubmitButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SubmitButton.css';
+import { useHistory } from "react-router-dom";
+import APIWrapper from '../../APIWrapper.js';
+
+
+function SubmitButton(props) {
+
+    let history = useHistory();
+    const APIKey = process.env.REACT_APP_211_API_KEY
+    const API = new APIWrapper(APIKey)
+
+
+    async function handleClick() {
+      await props.goBehavior();
+        if(props.isPageDataValid()){
+          await API.initialize()
+          await props.changeAPIData(API.getCategories());
+          history.push("/info");
+        }
+    }
+
+    return (
+      <button type="button" onClick={handleClick}>
+        Go
+      </button>
+    )
+}
+export default SubmitButton
